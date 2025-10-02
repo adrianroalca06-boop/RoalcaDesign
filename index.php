6de29a727ccccb6fa7e449355fcfe0e5ef35ac0d
adrianroalca06-boop <adrianroalca06@@gmail.com>
2025-10-02 18:24:41 +0200

    estilo

--- index.php
+++ index.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="Diseño.css">
 </head>
-<body style="text-align: center;">
+<header style="text-align: center;" id="Cabecera1">
     <?php
         echo "<h1>RoalcaDesign</h1>";
         echo "<p>Luxury matters.</p>";
     ?>
+</header>
+<body>
+    <h2 style="text-align: center;">Bienvenido a RoalcaDesign</h2>
+    <p style="text-align: center;">Aquí encontrarás los mejores diseños de lujo.</p>
+    <p style="text-align: center;">Explora nuestra colección y descubre la elegancia en cada detalle.</p>
 </body>
 </html>
