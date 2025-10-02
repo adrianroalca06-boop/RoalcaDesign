5aca73542f6ae58e2b79bcf9278bc4af0983642d
adrianroalca06-boop <adrianroalca06@@gmail.com>
2025-10-02 18:10:29 +0200

    Empezando

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="text-align: center;">
+    <?php
+        echo "<h1>RoalcaDesign</h1>";
+        echo "<p>Luxury matters.</p>";
+    ?>
+</body>
+</html>
